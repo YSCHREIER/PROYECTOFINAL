3f5fc48a5f006cd90a7ecbafcc6ff7ecb7f42f73
YSCHREIER <yolanda.schreier@gmail.com>
2023-05-09 20:34:31 +0000

    PRIMER COMMIT

--- webapp/controller/VerEmpleados.controller.js
+++ webapp/controller/VerEmpleados.controller.js
@@ -3,33 +3,90 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
-	"sap/ui/model/json/JSONModel"
-], function (Controller,Filter,FilterOperator,JSONModel) {
+	"sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox"
+], function (Controller,Filter,FilterOperator,JSONModel,MessageBox) {
 
     return Controller.extend("gestionempleados.controller.VerEmpleados", {
 
         onInit: function () {
             
+            this.sIdEmployee="";
+            this.sSapId="";
+            this.sPath="";
         },
 
         onSelectionEmployee:function(oEvent)
         {
             let oBindingContext = oEvent.getSource().getBindingContext("odataModelZEmployees"),
                 sIdEmployee = oBindingContext.getObject().EmployeeId,
+                sSAPID=oBindingContext.getObject().SapId,
                 sPath = oEvent.getSource().getBindingContext("odataModelZEmployees").getPath();
+
+                this.sIdEmployee=sIdEmployee;
+                this.sSAPID = sSAPID;
+                this.sPath = sPath;
 
                 let oDetail = this.getView().byId("detailEmployee");
                 oDetail.bindElement("odataModelZEmployees>"+ sPath);
-
                 
                 this.byId("splitAppEmployee").to(this.createId("detailEmployee"));
 
         },
 
+        onDeleteEmployee : function(){
+
+            var that = this;
+            let oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+           
+            
+           let sUrl = "/Users(EmployeeId='"+ this.sIdEmployee + "',SapId='"+this.sSAPID+"')"; 
+                      
+            MessageBox.confirm(oResourceBundle.getText("deleteConfirm"), {
+				actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+				onClose: function (oAction) {
+					if (oAction === MessageBox.Action.YES) {
+						
+                       
+                        this.getView().getModel("odataModelZEmployees").remove(sUrl,{
+                            success: function(){
+                                
+                                sap.m.MessageToast.show(oResourceBundle.getText("oDataDeleteOK"));
+                                this.byId("splitAppEmployee").to(this.createId("detailSelectEmployee"));
+
+                            }.bind(this),
+                            error : function(){
+                                sap.m.MessageToast.show(oResourceBundle.getText("oDataDeleteNOK"));
+                            }.bind(this)
+        
+                    });
+                       
+					}
+				}.bind(this)
+			});
+
+        },
+
+
+
+        readUploadSet : function (sIdEmployee, sSAPID) {
+
+        //Bind Files
+        this.byId("uploadCollection").bindAggregation("items", {
+            path: "odataModelZEmployees>/Attachments",
+            filters: [
+              
+                new Filter("SapId", FilterOperator.EQ, sSAPID),
+                new Filter("EmployeeId", FilterOperator.EQ, sIdEmployee),
+            ]
+        });
+
+       
+       },
+
+
         onSearchEmployee:function(oEvent)
         {
-
-          
 
             let sFiltro = oEvent.getSource().getValue();
             aFilters = [];
@@ -51,6 +108,86 @@ sap.ui.define([
 			var oListEmployee = this.byId("listEmployee");
 			var oBindingEmployee = oListEmployee.getBinding("items");
 			oBindingEmployee.filter(aFilters);
+
+        },
+
+        onFileUploadComplete : function (oEvent) {
+
+            var oUploadCollection = oEvent.getSource();
+            oUploadCollection.getBinding("items").refresh();
+
+        },
+
+        onFileUpload: function (oEvent) {
+            
+
+            let Token = this.getView().getModel("odataModelZEmployees").getSecurityToken(),
+            sSAPID = this.sSAPID,
+             oUploadSet = this.byId("uploadCollection"),
+             sEmployeeId = this.sIdEmployee;
+
+             oUploadSet.getItems().forEach(function (oItem) {
+                 if (oItem.getListItem().getSelected()) {
+               
+
+                    let  sFileName = oItem.getFileName(),
+                    sLug = sSAPID + ";" +  sEmployeeId + ";" + sFileName;
+            
+             
+
+            // Header Token CSRF - Cross-site request forgery
+            let oCustomerHeaderToken = new sap.ui.core.Item({
+                key: "X-CSRF-Token",
+                text:Token
+            });
+
+            oItem.addHeaderField(oCustomerHeaderToken);
+
+            let oCustomerText = new sap.ui.core.Item({
+                key:"Slug",
+                text:sLug
+            });
+
+            oItem.addHeaderField(oCustomerText)
+
+            oItem.setUploadState("Ready");
+            oUploadSet.setUploadUrl("/gestionempleados/sap/opu/odata/sap/ZEMPLOYEES_SRV/Attachments");
+            oUploadSet.uploadItem(oItem);
+              
+        }
+
+    });
+
+   
+    
+
+        },
+
+        
+        onFileDeleted: function (oEvent) {
+            var oUploadCollection = oEvent.getSource();
+            var sPath = oEvent.getParameter("item").getBindingContext("odataModelZEmployees").getPath();
+            this.getView().getModel("odataModelZEmployees").remove(sPath, {
+                success: function () {
+                    oUploadCollection.getBinding("items").refresh();
+                },
+                error: function () {
+
+                }
+            });
+        },
+
+        downloadFile : function(oEvent) {
+            
+            var oUploadSet = this.getView().byId("uploadCollection");
+
+            
+			oUploadSet.getItems().forEach(function (oItem) {
+				if (oItem.getListItem().getSelected()) {
+					oItem.download(true);
+				}
+			});
+            
 
         }
 
